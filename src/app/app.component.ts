422bb2e4c757d97eb5d26eebd31f6934ef77e0a0
rob <tdd@kepzes360>
2022-11-30 11:18:30 +0100

    portfolio as service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Portfolio } from './model/portfolio';
 import { Money } from './model/money';
+import { portfolioService } from './service/portfolio.service';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,19 @@ import { Money } from './model/money';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'currency-app';
-  portfolio: Portfolio = new Portfolio();
 
-  currency: string = '';
+  title = 'currency-app';
+  constructor(public portfolio: portfolioService){}
+
+  currency: string = 'HUF';
   amount: number = 0;
 
-  
+
   addMoney() {
-    const newMoney = new Money(this.amount, this.currency);
-    this.portfolio.add([newMoney]);
+    const newMoney = new Money(this.amount, this.currency)
+    this.portfolio.add([newMoney])
+    this.currency = 'HUF'
+    this.amount = 0
   }
 
 }
--- src/app/model/portfolio.ts
+++ src/app/model/portfolio.ts
@@ -1,24 +1,6 @@
 import {Money} from './money';
 
-export class Portfolio {
-    constructor() {}
-
-    money : Money[] = [];
-
-    add(money:Money[]) {
-        this.money = this.money.concat(money)
-    }
-    evaluate(currency:string){
-        let total = 0;
-        this.money.forEach(money => {
-            if(money.currency === currency){
-                total += money.amount;
-            }
-        });
-        return new Money(total, currency);
-    }
-    getMoney() {
-        return this.money.slice();
-    }
-
+export interface Portfolio {
+    add(money:Money[]): void
+    getMoney(): Money[]
 }
